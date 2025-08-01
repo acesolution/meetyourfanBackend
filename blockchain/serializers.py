083f631bfdec24e465517160d815642c43e010b4
Your Name <you@example.com>
2025-08-01 13:49:15 +0500

    updated serliazers

--- blockchain/serializers.py
+++ blockchain/serializers.py
@@ -18,7 +18,7 @@ class InfluencerTransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = InfluencerTransaction
         fields = [
-            'id', 'tx_hash', 'transaction_type', 'tt_amount', 'credits_delta',
+            'id', 'tx_hash', 'tx_type', 'tt_amount', 'credits_delta',
             'status', 'block_number', 'transaction_index',
             'gas_used', 'effective_gas_price',
             'from_address', 'to_address', 'value', 'input_data',
