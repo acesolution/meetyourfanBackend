c1177c05aea65c0bd3c31bded635090e2521c02d
Your Name <you@example.com>
2025-10-22 21:11:37 +0500

    updates register

--- api/signals.py
+++ api/signals.py
@@ -1,11 +1,15 @@
 # api/signals.py
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth import get_user_model
-from api.models import Profile
+from django.conf import settings
+from api.models import Profile  # Import the Profile model from the same app
 
-User = get_user_model()
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def ensure_profile(sender, instance, created, **kwargs):
-    Profile.objects.get_or_create(user=instance)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
